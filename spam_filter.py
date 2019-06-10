2a2ee467e45f9b8b89ce7264de1452f032e4e6c5
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-06-10 17:57:41 +0530

    UnicodeDecodeError ignored for now...
    - Word selection logic modified \n - Code formatted using flake8

--- spam_filter.py
+++ spam_filter.py
@@ -2,7 +2,6 @@
 
 import os
 
-import chardet
 import numpy as np
 from collections import Counter
 from sklearn.naive_bayes import MultinomialNB
@@ -11,45 +10,56 @@ from sklearn.metrics import confusion_matrix
 
 def make_Dictionary(train_dir):
     ''' Method to create Dictionary'''
-    emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]  # reads file names in directory and appends it with directory name
+    emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]  # reads file names in directory
     all_words = []
     for mail in emails:
-        with open(mail) as m:
-            mailstr = str(m)
-            charset=chardet.detect(mailstr)
-            for i,line in enumerate(m):   # enumerate reads mail and returns each line and its counter
-                if i == 2:		  # why 2? Should be >=2 if picking lines after subject.
-                    words = line.split()
-                    all_words += words
+        with open(mail, "r", encoding="us-ascii") as m:
+            try:
+                for i, line in enumerate(m):   # enumerate reads mail and returns each line and its counter
+                    if i == 2:		  # why 2? Should be >=2 if picking lines after subject.
+                        words = line.split()
+                        all_words += words
+            except UnicodeDecodeError:
+                pass
 
-    dictionary = Counter(all_words)	# Counts number of occurrences of words
+    dictionary = Counter(all_words)	  # Counts number of occurrences of words
 
     list_to_remove = dictionary.keys()
+    improvedDict = Counter()
+    i = 0
     for item in list_to_remove:
-        if item.isalpha() == False:
-            del dictionary[item]
-        elif len(item) == 1:
-            del dictionary[item]
-    dictionary = dictionary.most_common(3000)
-    return dictionary
+        if item.isalpha() is True:
+            improvedDict[i] = item
+            i = i+1
+        elif len(item) > 1:
+            improvedDict[i] = item
+            i = i+1
+    print("No. of Words processed: ", i)
+    improvedDict = improvedDict.most_common(3000)
+    return improvedDict
+
 
 def extract_features(mail_dir):
-    files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
-    features_matrix = np.zeros((len(files),3000)) 	#makes matrix of len(files) or 3000 containing all 0s
-    docID = 0;
+    files = [os.path.join(mail_dir, fi) for fi in os.listdir(mail_dir)]
+    features_matrix = np.zeros((len(files), 3000)) 	# makes matrix of len(files)x3000 containing all 0s
+    docID = 0
     for fil in files:
-      with open(fil) as fi:
-        for i,line in enumerate(fi):
-          if i == 2:			#why 2?
-            words = line.split()
-            for word in words:
-              wordID = 0
-              for i,d in enumerate(dictionary):
-                if d[0] == word:
-                  wordID = i
-                  features_matrix[docID,wordID] = words.count(word)
-        docID = docID + 1
+        try:
+            with open(fil) as fi:
+                for i, line in enumerate(fi):
+                    if i == 2:			# why 2?
+                        words = line.split()
+                        for word in words:
+                            wordID = 0
+                            for i, d in enumerate(dictionary):
+                                if d[0] == word:
+                                    wordID = i
+                                    features_matrix[docID, wordID] = words.count(word)
+                docID = docID + 1
+        except UnicodeDecodeError:
+            pass
     return features_matrix
+
 
 # Create a dictionary of words with its frequency
 
@@ -66,7 +76,7 @@ train_matrix = extract_features(train_dir)
 
 model1 = MultinomialNB()
 
-model1.fit(train_matrix,train_labels)	#Fit Naive Bayes classifier according to train_matrix and train_labels
+model1.fit(train_matrix, train_labels)  # Fit Naive Bayes classifier according to train_matrix and train_labels
 
 # Test the unseen mails for Spam
 
@@ -77,4 +87,4 @@ test_labels[130:260] = 1
 
 result1 = model1.predict(test_matrix)
 
-print(confusion_matrix(test_labels,result1))
+print(confusion_matrix(test_labels, result1))
