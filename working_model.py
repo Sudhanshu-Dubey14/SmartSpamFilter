3f6ddda653f783336a31436c47c97cb57e6ee83a
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-06-27 14:30:02 +0530

    Debugging feature removed

--- working_model.py
+++ working_model.py
@@ -11,13 +11,11 @@
 import sys
 
 from supporters.features import mail_features
-import numpy as np
 import pickle
 
 
 mail_file = sys.argv[1]
 features_matrix = mail_features(mail_file)
-np.savetxt("working_matrix.txt", features_matrix)
 
 ml_model = pickle.load(open('spamfilter.sav', 'rb'))
 result = ml_model.predict(features_matrix)
