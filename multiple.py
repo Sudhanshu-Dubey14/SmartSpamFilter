5c7bd53d37657073ffc744a817dd89530164524e
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-06-27 15:00:00 +0530

    Modified to print number of mails processed

--- multiple.py
+++ multiple.py
@@ -3,6 +3,7 @@ from supporters.working_model import predict
 
 
 def multiple(mail_dir):
+    i = 0
     emails = [os.path.join(mail_dir, f) for f in os.listdir(mail_dir)]  # reads file names in directory
     with open("result.txt", "w") as res:
         for mail in emails:
@@ -13,6 +14,8 @@ def multiple(mail_dir):
                 res.write(mail + " is normal mail.\n")
             else:
                 res.write("something went wrong for " + mail + "\n")
+            i = i + 1
+            print(i)
             print(mail + " is processesd.")
 
 
