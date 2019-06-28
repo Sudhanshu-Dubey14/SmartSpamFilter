b702fc5e035e3db0a843cc240cebe1ed4b7967da
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-06-28 14:56:30 +0530

    Ability to process html parts added

--- supporters/preprocessor.py
+++ supporters/preprocessor.py
@@ -1,5 +1,6 @@
 import spacy
 import email
+from bs4 import BeautifulSoup
 
 
 all_words = []
@@ -31,17 +32,43 @@ def split_payload(payload):
         if len(content) > 1000000:
             chunks, chunk_size = len(content), len(content)//999999
             for i in range(0, chunks, chunk_size):
-                get_words(content[i:i+chunk_size])
+                get_words_plain(content[i:i+chunk_size])
         else:
-            get_words(content)
+            get_words_plain(content)
+    elif content_subtype == "html":
+        content = payload.get_payload()
+        if len(content) > 1000000:
+            chunks, chunk_size = len(content), len(content)//999999
+            for i in range(0, chunks, chunk_size):
+                get_words_html(content[i:i+chunk_size])
+        else:
+            get_words_html(content)
 
 
-def get_words(content):
-    nlp = spacy.load("en_core_web_sm")
-    stopWords = spacy.lang.en.stop_words.STOP_WORDS
+def get_words_plain(content):
     nlpmail = nlp(content)
     for word in nlpmail:
         lemma = word.lemma_
         lemma = lemma.lower()
         if lemma.isalpha() and len(lemma) > 2 and len(lemma) < 10 and lemma not in stopWords:
             all_words.append(lemma)
+
+
+def get_words_html(content):
+    pure_html = BeautifulSoup(content, features="lxml")
+    for script in pure_html(["script", "style"]):
+        script.extract()
+    pure_text = pure_html.get_text()
+    lines = (line.strip() for line in pure_text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    pure_text = '\n'.join(chunk for chunk in chunks if chunk)
+    nlpmail = nlp(pure_text)
+    for word in nlpmail:
+        lemma = word.lemma_
+        lemma = lemma.lower()
+        if lemma.isalpha() and len(lemma) > 2 and len(lemma) < 20 and lemma not in stopWords:
+            all_words.append(lemma)
+
+
+nlp = spacy.load("en_core_web_sm")
+stopWords = spacy.lang.en.stop_words.STOP_WORDS
