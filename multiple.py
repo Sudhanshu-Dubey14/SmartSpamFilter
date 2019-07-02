46c743c044085ca95c92ffc57e4a2b6052182e14
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-07-02 17:50:50 +0530

    Documentation added

--- multiple.py
+++ multiple.py
@@ -1,8 +1,25 @@
+# -*- coding: utf-8 -*-
+
+##
+# \file spam_filter.py
+# \brief  Model building code
+# This code builds, trains and tests a new Machine Learning model
+# \author Sudhanshu Dubey
+# \version    1.0
+# \date   25/6/2019
+# \bug No known bugs
+
+
 import os
 from supporters.working_model import predict
 
 
 def multiple(mail_dir):
+    ##
+    # \brief   Method to predict results for all mails individually
+    # \param    mail_dir The directory containing mails
+    # \return   Nothing
+
     i = 0
     emails = [os.path.join(mail_dir, f) for f in os.listdir(mail_dir)]  # reads file names in directory
     with open("result.txt", "w") as res:
