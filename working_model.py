91f5a3c927b6878de0fb0a4f5593cc478bf1ecf4
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-07-02 17:51:11 +0530

    Documentation added

--- working_model.py
+++ working_model.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-"""
-@brief  Model implementing code
-@detail This code reads mails individually and the classifies them using pre-trained model
-@author Sudhanshu Dubey
-@version    1.0
-@date   25/6/19
+# \file working_model.py
+# \brief  Model implementing code
+# This code reads mails individually and the classifies them using pre-trained model
+# \author Sudhanshu Dubey
+# \version    1.0
+# \date   25/6/19
 
-"""
 import sys
 
 from supporters.features import mail_features
