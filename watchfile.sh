6a84c0280f1f966c22c6e40309a9ded93ce66527
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-07-09 14:34:11 +0530

    Modified to check for existence of watchlog

--- watchfile.sh
+++ watchfile.sh
@@ -4,6 +4,12 @@ CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WATCHDIR="$CURDIR/new_mails"
 WATCHLOG="$CURDIR/watchlog"
 
+if [ -f $WATCHLOG ]; then
+	rm $WATCHLOG
+	touch $WATCHLOG
+else
+	touch $WATCHLOG
+fi
+
 inotifywait -r -d $WATCHDIR -o $WATCHLOG --format %w%f -e create -e moved_to 
 python3 $CURDIR/fast_single.py $WATCHLOG
-echo "Spam filter started"
